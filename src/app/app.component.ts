aa08349a4712131f6f8d8c8e473735db16aa6320
Ricardo Almeida <ricardo2675@proton.me>
2025-02-11 01:55:09 +0000

    Created root app component

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: "app-root",
+    template: `<h1>Hello World!</h1>`,
+})
+export class AppComponent {
+    constructor() {
+        console.log("Hello World");
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-console.log("Hello World");
+import { AppComponent } from "./app/app.component.ts";
+import { bootstrapApplication } from "@angular/platform-browser";
+
+bootstrapApplication(AppComponent);
