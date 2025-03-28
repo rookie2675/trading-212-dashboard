9e831cb1325197cb6156458411197c459a00acb9
Ricardo Almeida <ricardo2675@proton.me>
2025-03-28 16:15:31 +0000

    Deleted tests because it was preventing gradle to run project

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,10 +21,4 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
 }
