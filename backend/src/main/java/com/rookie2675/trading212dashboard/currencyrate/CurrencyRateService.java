83c1073b51aed94233bbfd68c59d84bd48df58a3
Ricardo Almeida <ricardo2675@proton.me>
2025-03-29 14:11:58 +0000

    Completed CurrencyRateService

--- backend/src/main/java/com/rookie2675/trading212dashboard/currencyrate/CurrencyRateService.java
+++ backend/src/main/java/com/rookie2675/trading212dashboard/currencyrate/CurrencyRateService.java
@@ -1,12 +1,20 @@
 package com.rookie2675.trading212dashboard.currencyrate;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Map;
 
+@Service
 public class CurrencyRateService {
+
+    @Value("${app.security.free-currency-api.key}")
+    private String apiKey;
+
     private final RestTemplate restTemplate;
+
     private static final String URL = "https://api.freecurrencyapi.com/v1/latest";
 
     public CurrencyRateService() {
@@ -14,12 +22,16 @@ public class CurrencyRateService {
     }
 
     public Map<String, Double> fetch(String baseCurrency, String[] targetCurrencies) {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(URL)
+        String uriString = buildUriString(baseCurrency, targetCurrencies);
+        CurrencyRateApiResponse response = restTemplate.getForObject(uriString, CurrencyRateApiResponse.class);
+        return response.getData();
+    }
+
+    private String buildUriString(String baseCurrency, String[] targetCurrencies) {
+        return UriComponentsBuilder.fromHttpUrl(URL)
                 .queryParam("base_currency", baseCurrency)
                 .queryParam("currencies", String.join(",", targetCurrencies))
-                .queryParam("apikey", "");
-
-        CurrencyRateApiResponse response = restTemplate.getForObject(builder.toUriString(), CurrencyRateApiResponse.class);
-        return response.getData();
+                .queryParam("apikey", apiKey)
+                .toUriString();
     }
 }
