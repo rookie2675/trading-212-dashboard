1a31cc0bb2bd153574f137cfa44e8a4fa7347ba4
Ricardo Almeida <ricardo2675@proton.me>
2025-03-31 12:57:10 +0100

    Added CorsConfiguration to Spring project

---
+++ backend/src/main/java/com/rookie2675/trading212dashboard/configuration/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.rookie2675.trading212dashboard.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer configureCors() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
