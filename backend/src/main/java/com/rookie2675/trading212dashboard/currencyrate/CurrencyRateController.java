d24ab642df60bc3c9cd0df85520512080f777764
Ricardo Almeida <ricardo2675@proton.me>
2025-04-03 15:35:28 +0100

    Optimized imports

--- backend/src/main/java/com/rookie2675/trading212dashboard/currencyrate/CurrencyRateController.java
+++ backend/src/main/java/com/rookie2675/trading212dashboard/currencyrate/CurrencyRateController.java
@@ -1,7 +1,10 @@
 package com.rookie2675.trading212dashboard.currencyrate;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
