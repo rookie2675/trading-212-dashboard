f71fc8def08e12acdc2d185de9810249b7b791e0
Ricardo Almeida <ricardo2675@proton.me>
2025-04-03 15:35:21 +0100

    Converted the CurrencyRateApiResponse to a record

--- backend/src/main/java/com/rookie2675/trading212dashboard/currencyrate/CurrencyRateApiResponse.java
+++ backend/src/main/java/com/rookie2675/trading212dashboard/currencyrate/CurrencyRateApiResponse.java
@@ -2,14 +2,4 @@ package com.rookie2675.trading212dashboard.currencyrate;
 
 import java.util.Map;
 
-public class CurrencyRateApiResponse {
-    private Map<String, Double> data;
-
-    public Map<String, Double> getData() {
-        return data;
-    }
-
-    public void setData(Map<String, Double> data) {
-        this.data = data;
-    }
-}
+public record CurrencyRateApiResponse(Map<String, Double> data) { }
